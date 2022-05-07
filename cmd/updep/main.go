6ebe25985a4d70c73f4bc3f3ebcfed39f60142d0
cmaruejol <maruejolcedric@gmail.com>
2022-05-07 21:59:56 +0200

    Modifying gitignore Adding cmd/

---
+++ cmd/updep/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cedricmar/updep/pkg/tag"
+	"github.com/cedricmar/updep/pkg/updater"
+)
+
+func main() {
+	fs := updater.Flags{}
+	fs.Info = flag.Bool("i", false, "info mode")
+
+	flag.Usage = updater.Usage
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 1 {
+		fmt.Fprint(os.Stderr, "Error: Too many tags\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	t, err := tag.NewTag(args[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+	fs.Tag = t
+
+	updater.Update(fs)
+}
